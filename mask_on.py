35e09ecaa4be0efb5c32206902b3133c519f0177
jakob lambert-hartmann <jakob.lamberthartmann.canada@gmail.com>
2020-08-31 15:39:43 +0200

    restructured files locally

--- mask_on.py
+++ mask_on.py
@@ -7,12 +7,11 @@ import face_recognition
 from PIL import Image
 import numpy as np
 
-def transform_image(path):
-    img = face_recognition.load_image_file(path)
-    faces = face_recognition.face_locations(img, number_of_times_to_upsample=2)
-
+def mask_on(img, faces):
     out_img = Image.fromarray(img)
 
+    if verbose and len(faces) > 0:
+        print("%s faces recognized" % len(faces))
     for face in faces:
         t,r,b,l = face
 
@@ -20,6 +19,14 @@ def transform_image(path):
         m = mask.resize((r - l, h))
 
         out_img.paste(m, (l, b - h), m)
+    return out_img
+
+
+def transform_image(path):
+    img = face_recognition.load_image_file(path)
+    faces = face_recognition.face_locations(img, number_of_times_to_upsample=2)
+
+    out_img = mask_on(img, faces)
 
     out_img.save("out.png")
 
@@ -28,26 +35,16 @@ def transform_video(path):
     def mask_faces(get_frame, t):
         image = get_frame(t)
 
-
-        x = int(t*4)
+        x = int(t*8)
         if x not in mask_locations:
             faces = face_recognition.face_locations(image, number_of_times_to_upsample=2)
+            mask_locations[x] = faces
         elif len(mask_locations[x]) == 0:
             return image
         else:
             faces = mask_locations[x]
 
-        out_img = Image.fromarray(image)
-        if verbose and len(faces) > 0:
-            print("%s faces recognized" % len(faces))
-        for face in faces:
-            t,r,b,l = face
-
-            h = int(2/3 * (b - t))
-            m = mask.resize((r - l, h))
-
-            out_img.paste(m, (l, b - h), m)
-
+        out_img = mask_on(image, faces)
         return np.array(out_img)
         
 
@@ -58,10 +55,8 @@ def transform_video(path):
 
 
 
-
 if len(sys.argv) < 2:
     print("you forgot to specify a file, can't do much without one")
-
 
 verbose = False
 mask = Image.open("mask.png")
